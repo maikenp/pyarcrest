3e13cb48b2db90a2e938855fc7ecb42eead73c2f
jakobmerljak <merljak.jakob@gmail.com>
2023-06-15 13:11:32 +0200

    Remove unneded variable

--- src/pyarcrest/common.py
+++ src/pyarcrest/common.py
@@ -1,10 +1,6 @@
 import logging
 
 
-# taken from http.client default blocksize
-HTTP_BUFFER_SIZE = 8192
-
-
 def getNullLogger():
     logger = logging.getLogger('null')
     if not logger.hasHandlers():
